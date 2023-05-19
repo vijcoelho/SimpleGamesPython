608ef3559cf5931a0d510735e4697398ef8f3ce5
Vitor Coelho <vijcoelho@gmail.com>
2023-05-18 21:14:03 -0300

    a

---
+++ hangmanGame.py
@@ -0,0 +1,23 @@
+import random as rd
+
+def write_a_string():
+    global word
+    with open("C:/Users/u23623/Documents/GitHub/SimpleGamesPython/hangman.txt", "a") as txt:
+        word = input("Put a random word: ")
+        txt.writelines(f"\n{word}")
+    
+def pick_a_string():
+    search = input("search a word:")
+    with open("C:/Users/u23623/Documents/GitHub/SimpleGamesPython/hangman.txt", "r") as txt:
+        line = txt.readlines()
+        find = False
+        for i, line in enumerate(line):
+            if search in line:
+                find = True
+                print(line)
+
+
+pick_a_string()
+
+            
+
