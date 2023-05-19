dc5bdc447ae1855f25896799a3dc25d92adec5f4
Vitor Coelho <vijcoelho@gmail.com>
2023-05-18 22:12:07 -0300

    hangman game not finished

--- hangmanGame.py
+++ hangmanGame.py
@@ -1,24 +1,30 @@
 import random as rd
 
-# THIS CODE IS NOT FINISHED YET
+# THIS CODE IS NOT FINISHED 
 def write_a_string():
+    
     global word
     with open("C:/Users/u23623/Documents/GitHub/SimpleGamesPython/hangman.txt", "a") as txt:
-        word = input("Put a random word: ")
-        txt.writelines(f"\n{word}")
-    
+        word = input("Put a word: ")
+        word_code = input("Put a code for this word: ")
+        txt.writelines(f"\n{word + word_code}")
+
 def pick_a_string():
-    search = input("search a word:")
-    with open("C:/Users/u23623/Documents/GitHub/SimpleGamesPython/hangman.txt", "r") as txt:
+
+    search = input("search a number for find a word:")
+    with open("C:/Users/u23623/Documents/GitHub/SimpleGamesPython/hangman.txt", "r+") as txt:
         line = txt.readlines()
-        find = False
         for i, line in enumerate(line):
             if search in line:
-                find = True
-                print(line)
+                new_word = line[:-1]
+                print(new_word)
 
 
-pick_a_string()
 
-            
+ops = input("If u want write a word PRESS 1, but if u want pick a random word PRESS 2.: ")
+if ops == "1":
+    write_a_string()
+elif ops == "2":      
+    pick_a_string()
+                
 
