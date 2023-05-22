e56583d41ee52775e4916e1e440a1811d1f160f8
Vitor Coelho <vijcoelho@gmail.com>
2023-05-22 20:01:30 -0300

    up hangman

--- hangmanGame.py
+++ hangmanGame.py
@@ -1,9 +1,8 @@
 import random as rd
-
-# THIS CODE IS NOT FINISHED 
+ 
 def write_a_word(): 
     global word
-    word = input("\nPut a word: ")
+    word = "macarrao"
 
 def main():
     global word
@@ -11,9 +10,18 @@ def main():
         print("_", end=" ")
 
 def verification():
-
+    global anwser
+    anwser = ["_"] * len(word)
+    for i in range(6):
+        chance = input("\nPut a letter: ")
+        for i, letter in enumerate(word):
+            if letter == chance:
+                anwser[i] = letter
+                result = " ".join(anwser)
+        print(result)
+    print(f"Your chance is over, the anwser was {word}, try again!!")    
 
 write_a_word()
 main()
-
+verification()
 
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 palavra = input(":")
 for letra in palavra:
     print("_", end=" ")
+
+print("\n")
+
+resposta = ["_"] * len(palavra)
+tentativa = input("Coloque uma letra:")
+for i, letra in enumerate(palavra):
+    if letra == tentativa:
+        resposta[i] = letra
+resultado = " ".join(resposta)
+print(resultado)
+
+        
+
