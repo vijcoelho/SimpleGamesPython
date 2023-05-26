28897b84410b883890033c9a705519b128603c3b
Vitor Coelho <vijcoelho@gmail.com>
2023-05-25 21:11:21 -0300

    Update BattleShip.py

--- BattleShip.py
+++ BattleShip.py
@@ -1,10 +1,4 @@
 class Ship:
-    # def __init__(self, ship, destroyer, submarine, aircraft_carrier):
-    #     self.ship = ship
-    #     self.destroyer = destroyer
-    #     self.submarine = submarine
-    #     self.aircraft_carrier = aircraft_carrier
-    
     def __init__(self, name, size, qtd):
         self.name = str(name)
         self.size = size
