7502d6ebece8270e8cdf7dd4c823cb13e6953ea9
Vitor Coelho <vijcoelho@gmail.com>
2023-05-29 00:58:16 -0300

    up

--- BattleShip.py
+++
@@ -1,9 +0,0 @@
-class Ship:
-    def __init__(self, name, size, qtd):
-        self.name = str(name)
-        self.size = size
-        self.qtd = qtd
-
-    def getQtd(self):
-        return self.qtd
-        
--- mainBS.py
+++
@@ -1,6 +0,0 @@
-from BattleShip import Ship
-if __name__ == '__main__':
-    destroyer = Ship("Destroyer", 5, 2)
-    submarine = Ship("Submarine", 2, 3)
-    aircraft_carrier = Ship("Aircraft Carrier", 6, 1)
-    
